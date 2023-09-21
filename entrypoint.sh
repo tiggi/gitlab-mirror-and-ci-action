aefd149309ae0e586ef87e860a34f53a1af6ce61
Ulf 'Tiggi' Tigerstedt <ulf.tigerstedt@csc.fi>
2023-09-21 09:38:04 +0300

    add debug #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,6 @@ git checkout "$GITHUB_REF_NAME"
 branch="$(git symbolic-ref --short HEAD)"
 branch_uri="$(urlencode ${branch})"
 
-sh -c "git config --global --add safe.directory $PWD"
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
