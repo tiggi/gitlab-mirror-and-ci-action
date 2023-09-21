32c5c3eae8cb813bae15b897490522978c9cf375
Ulf 'Tiggi' Tigerstedt <ulf.tigerstedt@csc.fi>
2023-09-21 08:16:22 +0300

    Add check that GITLAB_PASSWORD is actually set

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,13 +21,18 @@ urlencode() (
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
+if [ -z ${GITLAB_PASSWORD+x} ]; then
+	echo ERROR: GITLAB_PASSWORD is not configured properly.
+	exit 1
+fi
+
 git checkout "${GITHUB_REF:11}"
 
 branch="$(git symbolic-ref --short HEAD)"
 branch_uri="$(urlencode ${branch})"
 
+sh -c "git config --global --add safe.directory $PWD"
 sh -c "git config --global credential.username $GITLAB_USERNAME"
-sh -c "git config --global --add safe.directory /github/workspace"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
